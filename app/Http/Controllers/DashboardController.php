3f69c962a90b1061b5cb379cbac40ec039e04f34
unknown <tahanasrollahii@gmail.com>
2024-03-18 11:36:23 -0800

    general commit

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Idea;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+
+class DashboardController extends Controller
+{
+
+    public function index()
+    {
+        $ideas = Idea::when(request()->has('search'), function ($query) {
+            $query->search(request('search', ''));
+        })->orderBy('created_at', 'DESC')->paginate(5);
+
+
+        return view('dashboard', [
+            'ideas' => $ideas
+        ]);
+    }
+}
